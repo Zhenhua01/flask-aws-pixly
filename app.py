e521eddebbaf230a8ac34c3e04be18ad01cd6967
Jordan Wong <pjwong97@gmail.com>
2022-09-14 17:59:25 -0700

    complete some features

--- app.py
+++ app.py
@@ -6,12 +6,13 @@ from flask_debugtoolbar import DebugToolbarExtension
 from sqlalchemy.exc import IntegrityError
 from werkzeug.utils import secure_filename
 from models import Image, Image_Metadata, db, connect_db
-from forms import AddImageForm
+from forms import AddImageForm, EditImageForm, EditImageForUploadForm
 from botocore.exceptions import ClientError
 
-
+import requests
 import boto3
-from PIL import ExifTags, Image as PilImage
+from io import BytesIO
+from PIL import ExifTags, Image as PilImage, ImageFilter
 from PIL.ExifTags import TAGS
 
 
@@ -51,6 +52,7 @@ BUCKET = os.environ['BUCKET_NAME']
 
 # boto3 client functions/helpers, import .env var
 
+
 @app.get('/')
 def homepage():
     """Display home page with list of top 20 images."""
@@ -66,9 +68,11 @@ def add_image():
 
     if form.validate_on_submit():
         f = form.photo.data
+
         filename = secure_filename(f.filename)
         f.save(os.path.join(filename))
-        with open(filename, "rb") as photo:
+        with open(filename, 'rb') as photo:
+            # print(type(photo))
             s3.upload_fileobj(photo, BUCKET, filename)
 
         # delete image form app or use uploadfileobj
@@ -127,6 +131,72 @@ def show_image(id):
 
 @app.route('/image/<int:id>/edit', methods=['GET', 'POST'])
 def edit_image(id):
+    image = Image.query.get_or_404(id)
+
+    form = EditImageForm()
+
+    filename = image.amazon_file_path
+    print('refreshing here')
+    response = requests.get(filename)
+    img = PilImage.open(BytesIO(response.content))
+    img.save("static/images/edit.JPG")
+    size = img.size
+    if form.validate_on_submit():
+
+        print("formData", form.black_and_white.data)
+        print("img is", img)
+        print("edge-detect", form.edge_detection.data)
+        if form.black_and_white.data == True:
+            print("did something here 2")
+            img = img.convert('L')
+        if "Smooth" in form.edge_detection.data:
+            img = img.convert('L')
+            img = img.filter(ImageFilter.SMOOTH)
+        if "Edges" in form.edge_detection.data:
+            img = img.filter(ImageFilter.FIND_EDGES)
+        if "Enhance" in form.edge_detection.data:
+            img = img.filter(ImageFilter.EDGE_ENHANCE)
+        if form.reduce.data > 0:
+            print("reduce by", form.reduce.data)
+            img = img.reduce(form.reduce.data)
+        size = img.size
+        img.save("static/images/edit.JPG")
+        return render_template('edit_image.html', image=image, size=size, form=form)
+        # img.show() it works
+        # img.save to is edit
+        # redirect with new img
+
+    return render_template('edit_image.html', image=image, size=size, form=form)
 
 
-    return render_template('image.html')
+@app.route('/uploadedit', methods=['GET', 'POST'])
+def upload_edit_image():
+
+    form = EditImageForUploadForm()
+
+    if form.validate_on_submit():
+        filename = form.file_name.data
+        with open(os.path.join("static/images/edit.JPG"), 'rb') as photo:
+            # print(type(photo))
+            s3.upload_fileobj(photo, BUCKET, filename)
+
+        # delete image form app or use uploadfileobj
+
+        # insert image data to images table
+        image_name = form.image_name.data
+        uploaded_by = form.uploaded_by.data
+        notes = form.notes.data
+
+        image = Image(
+            image_name=image_name,
+            uploaded_by=uploaded_by,
+            notes=notes,
+            amazon_file_path=f"http://{BUCKET}.s3.us-west-1.amazonaws.com/{filename}"
+        )
+
+        db.session.add(image)
+        db.session.commit()
+
+        return redirect(f'/image/{image.id}')
+
+    return render_template('upload_edit.html', form=form)
--- forms.py
+++ forms.py
@@ -1,12 +1,17 @@
 """WTForms for Pixly."""
 
+from argparse import OPTIONAL
+from signal import valid_signals
+from typing import Optional
+from wsgiref.validate import validator
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField
-from wtforms.validators import DataRequired, Email, Length
+from wtforms import StringField, TextAreaField, SelectMultipleField, RadioField, SelectField
+from wtforms.validators import DataRequired, Email, Length, Optional
 from flask_wtf.file import FileField, FileRequired, FileAllowed
 
+
 class AddImageForm(FlaskForm):
-    """Form for adding users."""
+    """ Form for adding images. """
 
     photo = FileField('Image to upload:', validators=[
         FileRequired(),
@@ -20,3 +25,43 @@ class AddImageForm(FlaskForm):
 
     notes = TextAreaField('Notes:')
 
+
+class EditImageForm(FlaskForm):
+    """ Form for editing image. """
+
+    black_and_white = RadioField(
+        'Black & White',
+        choices=[(True, "yes"), (False, '')], coerce=bool)
+
+    edge_detection = SelectMultipleField(
+        'Edge Detection (Hold shift to select multiple)',
+        choices=[
+            ("Smooth", "Smooth"),
+            ("Edges", 'Edges'),
+            ("Enhance", "Enhance")
+        ])
+
+    reduce = SelectField(
+        'Scale Down by',
+        choices=[
+            (1, "1x"),
+            (2, "2x"),
+            (3, "3x"),
+            (4, "4x"),
+            (5, "5x")], coerce=int)
+
+
+class EditImageForUploadForm(FlaskForm):
+    """ Form for adding images. """
+
+    file_name = StringField('File Name:', validators=[
+        DataRequired(),
+        Length(max=50)])
+
+    image_name = StringField('Image Name:', validators=[
+        DataRequired(),
+        Length(max=50)])
+
+    uploaded_by = StringField('Uploaded by:', validators=[DataRequired()])
+
+    notes = TextAreaField('Notes:')
