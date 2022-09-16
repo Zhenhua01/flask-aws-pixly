1bd3f5225a35115ec4f6c0af4e08debc0ef447d4
Jordan Wong <pjwong97@gmail.com>
2022-09-16 11:05:25 -0700

    complete first test, discovered bug

---
+++ test_image_model.py
@@ -0,0 +1,75 @@
+"""Image model tests."""
+
+# run these tests like:
+#
+#    python -m unittest test_image_model.py
+from sqlalchemy.exc import IntegrityError
+from models import db, Image, Image_Metadata
+from unittest import TestCase
+from app import app
+import os
+os.environ['DATABASE_URL'] = "postgresql:///pixly_test"
+
+
+# from psycopg2 import errors
+
+# BEFORE we import our app, let's set an environmental variable
+# to use a different database for tests (we need to do this
+# before we import our app, since that will have already
+# connected to the database
+
+
+# Now we can import app
+
+
+# Create our tables (we do this here, so we only create the tables
+# once for all tests --- in each test, we'll delete the data
+# and create fresh new clean test data
+
+db.create_all()
+
+
+class MessageModelTestCase(TestCase):
+    def setUp(self):
+        Image_Metadata.query.delete()
+        Image.query.delete()
+
+        image = Image(
+            image_name="test",
+            uploaded_by="test_user",
+            notes="notes",
+            filename="testfile.jpg",
+            amazon_file_path=f"http://test.s3.us-west-1.amazonaws.com/testfile.jpg"
+        )
+
+        db.session.add(image)
+        db.session.commit()
+
+        self.image_id = image.id
+
+        self.client = app.test_client()
+
+    def tearDown(self):
+        db.session.rollback()
+
+    def test_image_model(self):
+        """test if image is created successfully"""
+
+        img1 = Image.query.get(self.image_id)
+
+        self.assertEqual(img1.filename, 'testfile.jpg')
+        self.assertEqual(img1.notes, 'notes')
+
+    def test_empty_text(self):
+        """ test image model error for empty image string"""
+
+        new_img = Image(
+            image_name=None,
+            uploaded_by="test_user",
+            notes="notes",
+            filename="testfile.jpg",
+            amazon_file_path=f"http://test.s3.us-west-1.amazonaws.com/testfile.jpg"
+        )
+        db.session.add(new_img)
+
+        self.assertRaises(IntegrityError, db.session.commit)
